9c13bb951209b009f8bf8793d5984ba717762e66
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-07 05:20:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 import TechStack from './Components/TechStack';
+import Diamonds from "./diamonds/Diamonds"
+import { TextureLoader, LinearFilter } from "three"
+import React, { Suspense, useEffect, useRef, useMemo } from "react"
+
+function Startup() {
+  const ref = useRef()
+  useFrame(() => (ref.current.material.opacity = lerp(ref.current.material.opacity, 0, 0.025)))
+  return <Plane ref={ref} color="#0e0e0f" position={[0, 0, 200]} scale={[100, 100, 1]} />
+}
 
 class App extends Component {
 
