18da61a56cbfa66d32ede8deca9579608131eec9
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-07 05:17:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Diamonds from "./diamonds/Diamonds"
+import React, { Suspense, useEffect, useRef, useMemo } from "react"
+import { Canvas, useLoader, useFrame } from "@react-three/fiber"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
