fd6aff8625f7c2cd1b1e71ed4d581350cd281555
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-08 03:22:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
-import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
-import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
-import TechStack from './Components/TechStack';
 import Diamonds from "./diamonds/Diamonds"
 import { TextureLoader, LinearFilter } from "three"
 import React, { Suspense, useEffect, useRef, useMemo } from "react"
@@ -55,10 +50,6 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
-        <TechStack />
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
