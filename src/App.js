01de030aa3d48e29942c272b9d38b4fd6e0e434b
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-08 03:27:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,35 +16,6 @@ function Startup() {
 }
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
-
-  }
-
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
-
   render() {
     return (
       <div className="App">
